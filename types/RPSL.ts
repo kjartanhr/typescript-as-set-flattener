b056eed7602404f0f4f59f5ba11984c0aba4d862
Kjartan Hrafnkelsson <kjartan@kjartan.io>
2024-02-12 03:09:15 +0000

    make brain happy

--- types/RPSL.ts
+++ types/RPSL.ts
@@ -1,17 +1,17 @@
-export type RPSLKeys = "as-set" | 
-    "descr" | 
-    "members" | 
-    "mbrs-by-ref" | 
-    "remarks" | 
-    "org" | 
-    "tech-c" | 
-    "admin-c" | 
-    "notify" | 
-    "mnt-by" | 
-    "mnt-lower" | 
-    "created" | 
-    "last-modified" | 
-    "source";
+export type RPSLKeys = "as-set" 
+    | "descr" 
+    | "members"
+    | "mbrs-by-ref"
+    | "remarks"
+    | "org"
+    | "tech-c"
+    | "admin-c"
+    | "notify"
+    | "mnt-by"
+    | "mnt-lower"
+    | "created"
+    | "last-modified"
+    | "source";
 
 export type RPSLKey = `${RPSLKeys}:`;
 
